c6dec5649b9243670182be266bb6f010cd62f7c2
ethan <eidrory@massart.edu>
2016-03-26 23:12:04 -0400

    test website. will start build soon

---
+++ app builds/ionic.io build/platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+     
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('analyticsCtrl', function($scope) {
+
+})
+   
+.controller('manualControllerCtrl', function($scope) {
+
+})
+   
+.controller('profileCtrl', function($scope) {
+
+})
+ 
