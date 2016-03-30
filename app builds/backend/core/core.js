577028ef4535e17dc7a908fbe080c9c8222788f7
ethan <eidrory@massart.edu>
2016-03-30 19:25:31 -0400

    test

---
+++ app builds/backend/core/core.js
@@ -0,0 +1,60 @@
+// //Lets require/import the HTTP module
+// var http = require('http');
+//
+// //Lets define a port we want to listen to
+// const PORT=3000;
+//
+// //We need a function which handles requests and send response
+// function handleRequest(request, response){
+//
+//  response.end('<html><head></head><body>It Works!! Path Hit: ' + request.url+'</body></html>');
+//
+//  }
+//
+// //Create a server
+// var server = http.createServer(handleRequest);
+//
+// //Lets start our server
+// server.listen(PORT, function(){
+//     //Callback triggered when server is successfully listening. Hurray!
+//     console.log("Server listening on: http://localhost:%s", PORT);
+// });
+//
+
+var mysql   = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'aquifer'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connection was made');
+  console.log('connected as id ' + connection.threadId);
+});
+
+
+var http = require('http');
+var port = 9000;
+
+var httpserver = http.createServer(function (req, resp) {
+// switch (req.method) {
+//   case "GET":
+//
+//     break;
+//     case "Post":
+//
+//       break;
+//   default:
+//
+//     break;
+
+
+});
+
+httpserver.listen(port);
